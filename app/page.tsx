eabd8aee1719defd3ce7f59c72ad5d91ce1eeeb6
DenisCarvalho10 <deniscarvalhoadv@yahoo.com>
2026-06-10 18:09:40 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Cobertura em Saúde</h1>
-      <p>Site em construção...</p>
-    </main>
-  );
-}
+export { default } from "../src/app/page";
